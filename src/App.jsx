26e3552bc067b5efd137775e8fbbfa56b507754a
Deepak patel <dp089901@gmail.com>
2024-09-23 14:41:56 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from "./pages/home.jsx";
+import About from "./pages/about.jsx";
+import Service from "./pages/service.jsx";
+import Contact from "./pages/contact.jsx";
+import Mainnavbar from "./components/navbar.jsx";
 
 const App = ()=>{
   return(
     <>
-        <Home></Home>
+      <BrowserRouter>
+      <Mainnavbar></Mainnavbar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}> </Route>
+          <Route path="/about" element={<About></About>}> </Route>
+          <Route path="/service" element={<Service></Service>}> </Route>
+          <Route path="/contact" element={<Contact></Contact>}> </Route>
+        </Routes>
+
+      </BrowserRouter>        
     </>
   )
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {Link} from 'react-router-dom';
+import "./navbar.css";
+
+const Mainnavbar = () => {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Link id="home" to="/"> Home </Link>
+            <Link id="home" to="/about"> About </Link>
+            <Link id="home" to="/service"> Service </Link>
+            <Link id="home" to="/contact"> Contact </Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Mainnavbar;
